716fdada8565504784241a99ce9b43bc54e30fe9
mqzgood <1040598539@qq.com>
2021-04-28 08:00:21 +0800

    updata

--- src/pages/website/index.js
+++ src/pages/website/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container,Row, Col} from 'react-bootstrap';
 import BreezeNav from '../layout/breeze-nav';
+import B_cfx from '../../static/image/breeze-cfx.svg';
 
 class WebSiteIndex extends React.Component {
     render() {
@@ -8,10 +9,13 @@ class WebSiteIndex extends React.Component {
             <div>
                 <BreezeNav />
                 <div>
+                    
                     <Container className="breeze-container">
                         <Row  className="justify-content-md-center">
                             <Col sm>1 of 2</Col>
-                            <Col  sm>2 of 2</Col>
+                            <Col  sm>
+                            <img src={B_cfx} className="App-View" alt="breeze-cfx" />
+                            </Col>
                         </Row>
                        
                     </Container>
